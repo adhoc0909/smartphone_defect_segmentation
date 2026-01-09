148162513e2e55656d15fdd74b311ef5738eefe4
Guk Hyun <moonstalker9010@gmail.com>
2026-01-09 16:17:53 +0900

    [feat]: augmentation.sh - to Aug 200

--- augmentation.sh
+++ augmentation.sh
@@ -3,7 +3,7 @@
 # 실험1
  python "src/segtool/augmentation.py" cli_augment_dataset \
     --input_dir="data/Mobile Phone Defect" \
-    --counts="oil:100,stain:100,scratch:100"
+    --counts="oil:200,stain:200,scratch:200"
 
 # 실험2
 # python "src/segtool/augmentation.py" cli_augment_dataset \
